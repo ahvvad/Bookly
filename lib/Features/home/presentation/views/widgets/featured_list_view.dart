018a1ac0991cec7a879b3ad4a11ef196736a90f4
ahvvad <ahvadd.k@gmail.com>
2025-01-26 20:10:34 +0200

    Fix Json Errors and setup desplay func

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/Core/widgets/custom_error_widget.dart';
+import 'package:bookly/Core/widgets/custom_loading.dart';
+import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'custom_book_image.dart';
 
@@ -7,17 +11,27 @@ class FeaturedBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: CustomBookImage(),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CustomBookImage(),
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is FeaturedBooksFailuer) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoading();
+        }
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class Bookly extends StatelessWidget {
         BlocProvider(
           create: (context) => FeaturedBooksCubit(
             getIt.get<HomeRepoImpl>(),
-          ),
+          )..fetchFeaturedBooks(),
         ),
         BlocProvider(
           create: (context) => NewsetBooksCubit(
